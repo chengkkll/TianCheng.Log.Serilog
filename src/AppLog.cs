1d5d051b87f2d8a8f52f33ec19d05d7b0c130702
cheng_kkll <cheng_kkll@163.com>
2020-04-01 23:11:04 +0800

    初始化

---
+++ src/AppLog.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace TianCheng.Log
+{
+    /// <summary>
+    /// 读取配置信息的日志
+    /// </summary>
+    public class AppLog
+    {
+        /// <summary>
+        /// 日志操作对象
+        /// </summary>
+        static private ILogger _Logger = null;
+        /// <summary>
+        /// 日志操作
+        /// </summary>
+        static public ILogger Logger
+        {
+            get
+            {
+                if (_Logger == null)
+                {
+                    InitLogger();
+                }
+                return _Logger;
+            }
+        }
+        /// <summary>
+        /// 默认的文件格式
+        /// </summary>
+        private static readonly string FileFormat = "Logs/app-{Date}.txt";
+
+        /// <summary>
+        /// 初始化日志
+        /// </summary>
+        static private void InitLogger()
+        {
+            try
+            {
+                var con = new LoggerConfiguration().ReadFrom.Configuration(BuildConfiguration());
+                _Logger = con.CreateLogger();
+            }
+            catch
+            {
+                _Logger = new LoggerConfiguration()
+                            .WriteTo.Console(Serilog.Events.LogEventLevel.Information)
+                            .WriteTo.Debug()
+                            .WriteTo.RollingFile(FileFormat)
+                            .CreateLogger();
+            }
+        }
+
+        /// <summary>
+        /// 创建一份配置文件信息
+        /// </summary>
+        /// <returns></returns>
+        public static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+    }
+}
---
+++ src/CommonLog.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+
+namespace TianCheng.Log
+{
+    /// <summary>
+    /// 通用的日志记录 默认配置
+    /// </summary>
+    public class CommonLog
+    {
+        /// <summary>
+        /// 日志操作对象
+        /// </summary>
+        static private Serilog.ILogger _Logger = null;
+        /// <summary>
+        /// 日志操作
+        /// </summary>
+        static public Serilog.ILogger Logger
+        {
+            get
+            {
+                if (_Logger == null)
+                {
+                    InitLogger();
+                }
+                return _Logger;
+            }
+        }
+        /// <summary>
+        /// 默认的文件格式
+        /// </summary>
+        private static readonly string FileFormat = "Logs/TianCheng.Common-{Date}.txt";
+
+        /// <summary>
+        /// 初始化日志
+        /// </summary>
+        static private void InitLogger() =>
+            _Logger = new LoggerConfiguration()
+                        .WriteTo.Console(Serilog.Events.LogEventLevel.Information)
+                        .WriteTo.Debug()
+                        .WriteTo.RollingFile(FileFormat, Serilog.Events.LogEventLevel.Warning)
+                        .CreateLogger();
+    }
+}
